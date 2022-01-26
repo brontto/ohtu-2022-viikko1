f2e9d96ba007e4fbbdca71daef8ca05bd0306464
brontto <bronttosaur@gmail.com>
2022-01-27 00:58:26 +0200

    testit ja muuta

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,38 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_virheellinen_tilavuus(self):
+        self.varasto = Varasto(-1)
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+    
+    def test_virheellinen_alku_saldo(self):
+        self.varasto = Varasto(10, -1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_virhellinen_saldo_suhteessa_tilavuuteen(self):
+        self.varasto = Varasto(10, 15)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+
+    def test_virheellinen_lisays(self):
+        self.varasto.lisaa_varastoon(-5)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_virheellinen_lisays_liikaa(self):
+        self.varasto.lisaa_varastoon(15)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_virheellinen_otto_maara_miinuksella(self):
+        self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_otetaan_yli(self):
+        self.varasto.lisaa_varastoon(5)
+        self.assertAlmostEqual(self.varasto.ota_varastosta(10),5)
+
+    def test_str(self):
+        self.assertEqual(self.varasto.__str__(),"saldo = 0, vielä tilaa 10")
+        
+    
+
